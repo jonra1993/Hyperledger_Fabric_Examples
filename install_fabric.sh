4734769ceabe27ca2456e0f91ac9c9fbf595319b
Jonathan <jon_ra@hotmail.es>
2019-03-10 11:58:34 -0500

    add prerequisites and install fabric binaries without default samples

---
+++ install_fabric.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Descargar todo en Fabric"
+# Bypass default samples
+curl -sSL http://bit.ly/2ysbOFE | bash -s 1.4.0 -s
+
+## For downloading default samples
+#curl -sSL http://bit.ly/2ysbOFE | bash -s 1.4.0
+
+
